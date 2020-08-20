fd8d36908ead8f0493e5cfd7f094a33ed5fada1b
Oliver Carter <oliverc101@hotmail.com>
2020-08-20 23:43:51 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from player import *
+
+
+def main():
+    win = pygame.display.set_mode((800, 600))
+    clock = pygame.time.Clock()
+    player = Player()
+    sprites.add(player)
+
+    running = True
+    while running:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                running = False
+
+        sprites.update()
+
+        win.fill((220, 220, 100))
+        sprites.draw(win)
+
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    pygame.init()
+    main()
