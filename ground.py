13c544073d4cffd4ec116bc540d0d92811ab6d9d
Oliver Carter <oliverc101@hotmail.com>
2020-08-23 22:12:51 +0100

    Update ground.py

--- ground.py
+++ ground.py
@@ -1,23 +1,23 @@
 import pygame
+from random import randint
 
-platforms = pygame.sprite.Group()
+blocks = pygame.sprite.Group()
+chunks = pygame.sprite.Group()
 
 
-class Platform(pygame.sprite.Sprite):
+class Block(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((400, 40))
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((50, 200, 75))
         self.rect = self.image.get_rect()
         self.rect.bottom = 600
 
     def __str__(self):
         return 'Platform'
 
-    def update(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.image.scroll(1, 1)
 
-        if keys[pygame.K_RIGHT]:
-            self.image.scroll(dx=1)
-
+class Chunk(object):
+    def __init__(self):
+        self.surface = pygame.Surface((400, 250))
+        self.noise = [[randint(1, 3)] for i in range(8)]
