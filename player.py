a69c4450b27f8881ed985cde2f9a2d3aa1527a6a
Oliver Carter <oliverc101@hotmail.com>
2020-08-23 22:12:35 +0100

    Update player.py

--- player.py
+++ player.py
@@ -6,7 +6,8 @@ sprites = pygame.sprite.Group()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((40, 40))
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((255,50,255))
         self.rect = self.image.get_rect()
         self.velocity = 10
 
@@ -18,15 +19,14 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.rect.centery -= self.velocity
 
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.rect.centery += self.velocity
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rect.centerx -= self.velocity
 
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rect.centerx += self.velocity
-
