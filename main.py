fdf2bf4e4e2e5fc962567c73c47d1081ad737869
Oliver Carter <oliverc101@hotmail.com>
2020-08-23 14:17:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import pygame
 from player import *
+from ground import *
 
 
 def main(*args, **kwargs):
-    win = pygame.display.set_mode((800, 600))
+    win = pygame.display.set_mode((800, 600), 0, 32)
     clock = pygame.time.Clock()
     player = Player()
+    surf = pygame.Surface((400, 300))
+    platforms.add(Platform())
     sprites.add(player)
 
     running = True
     while running:
-        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 running = False
 
         sprites.update()
+        platforms.update()
 
         win.fill((220, 220, 100))
-        sprites.draw(win)
+
+        platforms.draw(win)
 
         pygame.display.update()
+        clock.tick(60)
 
 
 if __name__ == '__main__':
